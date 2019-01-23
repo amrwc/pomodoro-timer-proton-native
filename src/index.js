4adf28b4209149492e004eb7c7c14953e5c1c2cd
amrwc <42574496+amrwc@users.noreply.github.com>
2019-01-23 19:50:14 +0000

    Create a basic-looking Pomodoro Timer in Proton Native

---
+++ src/index.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { render, Window, App, Box, Button, StyledText } from 'proton-native';
+
+const POMODORO_LEN = 1500,
+  LONG_BREAK_LEN = 600,
+  SHORT_BREAK_LEN = 300;
+
+class PomodoroTimer extends Component {
+  state = {
+    timeInSeconds: POMODORO_LEN
+  };
+
+  componentWillMount() {
+    this.refreshTimer();
+  }
+
+  getTime = () => {
+    const { minutes, seconds } = this.state;
+    return `${minutes}:${seconds}`;
+  };
+
+  startTimer = () => {
+    if (!this.intervalId && this.state.timeInSeconds > 0) {
+      this.intervalId = setInterval(() => {
+        this.updateTime();
+        this.refreshTimer();
+      }, 1000);
+    }
+  };
+
+  stopTimer = () => {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  };
+
+  resetTimer = (newTime = POMODORO_LEN) => {
+    this.stopTimer();
+    this.setState({ timeInSeconds: newTime });
+    this.refreshTimer();
+  };
+
+  updateTime = () => {
+    const { timeInSeconds } = this.state;
+
+    if (timeInSeconds > 0) {
+      this.setState({ timeInSeconds: timeInSeconds - 1 });
+    } else {
+      this.stopTimer();
+    }
+  };
+
+  refreshTimer = () => {
+    const { timeInSeconds } = this.state;
+
+    let minutes = Math.floor(timeInSeconds / 60);
+    // Add 0 if minutes less than 10.
+    minutes = minutes <= 9 ? `0${minutes}` : minutes;
+
+    let seconds = timeInSeconds % 60;
+    // Add 0 if seconds less than 10.
+    seconds = seconds <= 9 ? `0${seconds}` : seconds;
+
+    this.setState({ minutes, seconds });
+  };
+
+  render() {
+    return (
+      <App>
+        <Window
+          title="Pomodoro Timer"
+          size={{ w: 500, h: 300 }}
+          menuBar={false}
+        >
+          <Box padded={true}>
+            <StyledText style={{ fontSize: 50 }}>{this.getTime()}</StyledText>
+            <Box>
+              <Button onClick={this.startTimer}>Start</Button>
+              <Button onClick={this.stopTimer}>Pause</Button>
+              <Button onClick={() => this.resetTimer()}>Reset</Button>
+              <Button
+                onClick={() => {
+                  this.resetTimer(SHORT_BREAK_LEN);
+                  this.startTimer();
+                }}
+              >
+                Short Break
+              </Button>
+              <Button
+                onClick={() => {
+                  this.resetTimer(LONG_BREAK_LEN);
+                  this.startTimer();
+                }}
+              >
+                Long Break
+              </Button>
+            </Box>
+          </Box>
+        </Window>
+      </App>
+    );
+  }
+}
+
+render(<PomodoroTimer />);
