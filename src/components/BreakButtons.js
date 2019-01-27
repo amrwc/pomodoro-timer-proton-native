bd51ee8e35e71d77eaa482f57b8fe04f8a56089d
amrwc <42574496+amrwc@users.noreply.github.com>
2019-01-27 00:20:16 +0000

    Improve the timer's accuracy
    - improve the timer's accuracy by using Date object,
    - refactor small snippets, clean up the code in general.
    
    The timer is now accurate, but still does not offer notifications.

--- src/components/BreakButtons.js
+++ src/components/BreakButtons.js
@@ -1,28 +1,19 @@
 import React from 'react';
 import { Box, Button } from 'proton-native';
 
-const LONG_BREAK_LEN = 600,
-  SHORT_BREAK_LEN = 300;
+const LONG_BREAK_LEN = 600;
+const SHORT_BREAK_LEN = 300;
 
 const BreakButtons = props => {
+  const startBreak = len => {
+    props.resetTimer(len);
+    props.startTimer();
+  };
+
   return (
     <Box padded={true} vertical={false}>
-      <Button
-        onClick={() => {
-          props.resetTimer(SHORT_BREAK_LEN);
-          props.startTimer();
-        }}
-      >
-        Short Break
-      </Button>
-      <Button
-        onClick={() => {
-          props.resetTimer(LONG_BREAK_LEN);
-          props.startTimer();
-        }}
-      >
-        Long Break
-      </Button>
+      <Button onClick={() => startBreak(SHORT_BREAK_LEN)}>Short Break</Button>
+      <Button onClick={() => startBreak(LONG_BREAK_LEN)}>Long Break</Button>
     </Box>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { render, Window, App, Box } from 'proton-native';
+import { render, App, Window, Box } from 'proton-native';
 
 import Timer from './components/Timer';
 import ControlButtons from './components/ControlButtons';
@@ -8,83 +8,80 @@ import BreakButtons from './components/BreakButtons';
 const POMODORO_LEN = 1500;
 
 class PomodoroTimer extends Component {
-  state = {
-    timeInSeconds: POMODORO_LEN
-  };
+  state = { timeInSeconds: POMODORO_LEN };
 
   componentWillMount() {
     this.refreshTimer();
   }
 
   startTimer = () => {
-    if (!this.intervalId && this.state.timeInSeconds > 0) {
+    const { timeInSeconds } = this.state;
+
+    if (!this.intervalId && timeInSeconds > 0) {
+      // All numbers in this scope are miliseconds [ms].
+      this.setState({ startTime: Date.now() });
+
+      // https://stackoverflow.com/a/29972322/10620237
       this.intervalId = setInterval(() => {
-        this.updateTime();
+        const elapsedTime = Date.now() - this.state.startTime;
+        const newTime = timeInSeconds - Math.floor(elapsedTime / 1000);
+
+        this.updateTime(newTime);
         this.refreshTimer();
-      }, 10);
+      }, 100);
     }
   };
 
   stopTimer = () => {
     if (this.intervalId) {
       clearInterval(this.intervalId);
-      this.intervalId = null;
+      this.intervalId = null; // This line helps startTimer's condition.
     }
   };
 
-  resetTimer = (newTime = POMODORO_LEN) => {
+  resetTimer = (timeInSeconds = POMODORO_LEN) => {
     this.stopTimer();
-    this.setState({ timeInSeconds: newTime });
+    this.setState({ timeInSeconds, startTime: undefined });
     this.refreshTimer();
   };
 
-  updateTime = () => {
-    const { timeInSeconds } = this.state;
-
-    if (timeInSeconds > 0) {
-      this.setState({ timeInSeconds: timeInSeconds - 0.01 });
-    } else {
-      this.stopTimer();
-    }
+  updateTime = newTime => {
+    this.state.timeInSeconds > 0
+      ? this.setState({ timeInSeconds: newTime })
+      : this.stopTimer();
   };
 
   refreshTimer = () => {
     const { timeInSeconds } = this.state;
-
-    let minutes = Math.floor(Math.floor(timeInSeconds) / 60);
-    // Add 0 if minutes less than 10.
+    let minutes = Math.floor(timeInSeconds / 60);
+    let seconds = timeInSeconds % 60;
+    // Prepend 0 if less than 10.
     minutes = minutes <= 9 ? `0${minutes}` : minutes;
-
-    let seconds = Math.floor(timeInSeconds) % 60;
-    // Add 0 if seconds less than 10.
     seconds = seconds <= 9 ? `0${seconds}` : seconds;
 
     this.setState({ minutes, seconds });
   };
 
   render() {
-    const { minutes, seconds } = this.state,
-      windowTitle = `Pomodoro Timer ${minutes}:${seconds}`;
+    const { minutes, seconds } = this.state;
+    const windowProps = {
+      title: `Pomodoro Timer ${minutes}:${seconds}`,
+      size: { w: 500, h: 300 },
+      menuBar: false,
+      margined: true
+    };
+    const btnProps = {
+      startTimer: this.startTimer,
+      resetTimer: this.resetTimer
+    };
 
     return (
       <App>
-        <Window
-          title={windowTitle}
-          size={{ w: 500, h: 300 }}
-          menuBar={false}
-          margined
-        >
+        <Window {...windowProps}>
           <Box padded={true}>
             <Timer minutes={minutes} seconds={seconds} />
-            <ControlButtons
-              startTimer={this.startTimer}
-              stopTimer={this.stopTimer}
-              resetTimer={this.resetTimer}
-            />
-            <BreakButtons
-              startTimer={this.startTimer}
-              resetTimer={this.resetTimer}
-            />
+            <ControlButtons {...btnProps} stopTimer={this.stopTimer} />
+            <BreakButtons {...btnProps} />
           </Box>
         </Window>
       </App>
